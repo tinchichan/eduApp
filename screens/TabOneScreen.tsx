58e6368dcc6ed5b4ce4d59adbf152038a234b033
Sawa <chyeungam@connect.ust.hk>
2022-04-25 03:10:39 +0800

    fix: rn components style props

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,23 +1,37 @@
-
-import { StyleSheet} from 'react-native';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { StyleSheet } from "react-native";
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
 const bgImage = { uri: "https://reactjs.org/logo-og.png" };
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>About This App</Text>
-      <br></br>
-      <Text style={styles.para}>There are <b>three tabs</b> in this app.</Text>
-      <br></br>
-        <View>
-          <Text style={styles.para}><b>About</b>: Introductory Page of the App</Text>
-          <Text style={styles.para}><b>Schools</b>: Search All Hong Kong schools information here. First choose the district, then search for specific info you prefer.</Text>
-          <Text style={styles.para}><b>Take Note</b>: Jot down school information if you find something useful.</Text>
-        </View>
+      <Text>{"\n"}</Text>
+      <Text style={styles.para}>
+        There are <Text style={{ fontWeight: "bold" }}>three tabs</Text> in this
+        app.
+      </Text>
+      <Text>{"\n"}</Text>
+      <View>
+        <Text style={styles.para}>
+          <Text style={{ fontWeight: "bold" }}>About</Text>: Introductory Page
+          of the App
+        </Text>
+        <Text style={styles.para}>
+          <Text style={{ fontWeight: "bold" }}>Schools</Text>: Search All Hong
+          Kong schools information here. First choose the district, then search
+          for specific info you prefer.
+        </Text>
+        <Text style={styles.para}>
+          <Text style={{ fontWeight: "bold" }}>Take Note</Text>: Jot down school
+          information if you find something useful.
+        </Text>
+      </View>
     </View>
   );
 }
@@ -25,17 +39,17 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightgrey'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightgrey",
   },
   title: {
     fontSize: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  para:{
-    textAlign: 'left',
+  para: {
+    textAlign: "left",
     fontSize: 20,
-    margin: 20
+    margin: 20,
   },
 });
